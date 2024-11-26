f105c6962a7726c2b11d7d6bc896d9e5b15b977d
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-11-26 11:39:04 +0600

    Setup the coffee store project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h3 className="text-4xl text-center text-red-500 font-bold">
+        Coffee Store
+      </h3>
+    </>
+  );
+}
+
+export default App;
