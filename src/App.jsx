e3f774ec30760951b2b8247af120c97ebff96770
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-11-27 12:32:43 +0600

    Added a coffee card to show coffee

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useLoaderData } from "react-router-dom";
 import "./App.css";
+import CoffeeCard from "./components/CoffeeCard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const coffees = useLoaderData();
   return (
-    <>
+    <div className="m-20">
       <h3 className="text-4xl text-center text-red-500 font-bold">
-        Coffee Store
+        Coffee Store: {coffees.length}
       </h3>
-    </>
+
+      <section className="grid md:grid-cols-2 gap-4">
+        {coffees.map((coffee) => (
+          <CoffeeCard key={coffee._id} coffee={coffee} />
+        ))}
+      </section>
+    </div>
   );
 }
 
---
+++ src/components/CoffeeCard.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Swal from "sweetalert2";
+
+const CoffeeCard = ({ coffee }) => {
+  const { _id, name, chef, supplier, taste, photo } = coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your Coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
+  return (
+    <div className="card card-side bg-base-100">
+      <figure className="w-[250px]">
+        <img className="rounded-lg w-full" src={photo} alt="" />
+      </figure>
+      <div className="flex justify-between items-center w-full pl-2 pr-4 text-left">
+        <div>
+          <h2 className="card-title">{name}</h2>
+          <p>Chef: {chef}</p>
+          <p>{supplier}</p>
+          <p>{taste}</p>
+        </div>
+        <div className="card-actions justify-end">
+          <div className="join join-vertical space-y-4">
+            <button className="btn">View</button>
+            <button className="btn">Edit</button>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="btn bg-orange-500"
+            >
+              X
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CoffeeCard;
